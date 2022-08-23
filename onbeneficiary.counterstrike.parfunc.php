d453fc3b304ffc97eee153a5f4bc59693c4d51a9
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-24 04:16:34 +1000

    Added new files

--- onbeneficiary.counterstrike.parfunc.php
+++ onbeneficiary.counterstrike.parfunc.php
@@ -1,71 +1,29 @@
 <?php
 
-if ($subHandle != '') {
-    $subActions = ["bear", "steal", "drop"];
-    $subActionCount = count($subActions);
-    $subAction = $subActions[rand(0, $subActionCount - 1)];
-    if ($subAction == 'bear') {
-        if (file_exists($subHandle)) {
-            $hanNotation = useNotation($subHandle);
-            $hanCoord = file_get_contents($subHandle.'/coord');
-            $hanX = explode(';', $hanCoord)[0];
-            $hanY = explode(';', $hanCoord)[1];
-            $hanZ = explode(';', $hanCoord)[2];
-            $msgBox = synchrone($turnNum, $subNotation, $subX, $subY, $subZ, $hanNotation, $hanX, $hanY, $hanZ, 3, $subMove);
-            $subX = $msgBox['i']['x'];
-            $subY = $msgBox['i']['y'];
-            $subZ = $msgBox['i']['z'];
-            $hanX = $msgBox['j']['x'];
-            $hanY = $msgBox['j']['y'];
-            $hanZ = $msgBox['j']['z'];
-            $subScore += 5;
-            file_put_contents($han.'/coord', $hanX.';'.$hanY.';'.$hanZ);
-            chmod($han.'/coord', 0777);
-        } else {
-            $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subMove);
-            $subX = $msgBox['x'];
-            $subY = $msgBox['y'];
-            $subZ = $msgBox['z'];
-        }
-    } elseif ($subAction == 'drop') {
-        if (file_exists($subHandle)) {
-            $hanHalfNotation = useHalfNotation($subHandle);
-            $hanCoord = file_get_contents($subHandle.'/coord');
-            $subHandle = '';
-            echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]['drop'].' '.$hanHalfNotation.' '.$spacedictus[$proLingo]['on'].' {'.$hanCoord."}<br>";
-        }
-    } elseif ($subAction == 'steal') {
-        if (file_exists($subHandle)) {
-            $hanHalfNotation = useHalfNotation($subHandle);
-            $hanMoney = file_get_contents($subHandle.'/money');
-            echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]['steal']." ".$hanMoney.' '.$spacedictus[$proLingo]['from'].' '.$hanHalfNotation."<br>";
-            $hanMoney -= $hanMoney;
-            $subMoney += $subMoney;
-            $subScore += 10;
-            file_put_contents($han.'/money', $hanMoney);
-            chmod($han.'/money', 0777);
-        } else {
-            $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subMove);
-            $subX = $msgBox['x'];
-            $subY = $msgBox['y'];
-            $subZ = $msgBox['z'];
-        }
-    }
-} else {
-    $subActions = ["walk", "pickup"];
-    $subActionCount = count($subActions);
-    $subAction = $subActions[rand(0, $subActionCount - 1)];
-    if ($subAction == 'walk') {
-        $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subMove);
-        $subX = $msgBox['x'];
-        $subY = $msgBox['y'];
-        $subZ = $msgBox['z'];
-    } elseif ($subAction == 'pickup') {
-        $subHandle = $obj;
-        if (file_exists($subHandle)) {
-            $hanHalfNotation = useHalfNotation($subHandle);
-            $hanCoord = file_get_contents($subHandle.'/coord');
-            echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]['pickup'].' '.$hanHalfNotation.' '.$spacedictus[$proLingo]['on'].' {'.$hanCoord."}<br>";
-        }
-    }
+$subActions = ["walk", "heal", "escort", "vendor"];
+$subActionCount = count($subActions);
+$subAction = $subActions[rand(0, $subActionCount - 1)];
+
+if ($subAction == "walk") {
+    $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subMove);
+    $subX = $msgBox['x'];
+    $subY = $msgBox['y'];
+    $subZ = $msgBox['z'];
+} elseif ($subAction == "heal") {
+    $objRating += $subHeal;
+    $subScore += 5;
+    echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["heal"].' ('.$subHeal.') '.$objHalfNotation."<br>";
+} elseif ($subAction == "escort") {
+    $msgBox = synchrone($turnNum, $subNotation, $subX, $subY, $subZ, $objNotation, $objX, $objY, $objZ, 3, $subMove);
+    $subX = $msgBox['i']['x'];
+    $subY = $msgBox['i']['y'];
+    $subZ = $msgBox['i']['z'];
+    $objX = $msgBox['j']['x'];
+    $objY = $msgBox['j']['y'];
+    $objZ = $msgBox['j']['z'];
+    $subScore += 5;
+} elseif ($subAction == "vendor") {
+    $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proMoney, $subMoney, ratioCalc($proEconVal, $subEconVal), $proUseWeapon);
+    $proMoney = $msgBox['debit'];
+    $subMoney = $msgBox['credit'];
 }
