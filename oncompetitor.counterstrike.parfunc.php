0268cf9bd7738aded8f4927d8a0382f95bc4ba65
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-24 01:14:08 +1000

    Added new files

--- oncompetitor.counterstrike.parfunc.php
+++ oncompetitor.counterstrike.parfunc.php
@@ -1,6 +1,6 @@
 <?php
 
-$subActions = ["walk", "strike", "defend", "vendor", "withdraw"];
+$subActions = ["walk", "strike", "defend", "vendor"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
@@ -23,8 +23,4 @@ if ($subAction == "walk") {
     $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proMoney, $subMoney, ratioCalc($proEconVal, $subEconVal), $proUseWeapon);
     $proMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
-} elseif ($subAction == "withdraw") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subMoney, $proMoney, ratioCalc($subEconVal, $proEconVal), $subUseWeapon);
-    $subMoney = $msgBox['debit'];
-    $proMoney = $msgBox['credit'];
 }
--- onpartner.counterstrike.parfunc.php
+++ onpartner.counterstrike.parfunc.php
@@ -1,6 +1,6 @@
 <?php
 
-$subActions = ["walk", "heal", "escort", "vendor", "withdraw"];
+$subActions = ["walk", "heal", "escort", "vendor"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
@@ -26,8 +26,4 @@ if ($subAction == "walk") {
     $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proMoney, $subMoney, ratioCalc($proEconVal, $subEconVal), $proUseWeapon);
     $proMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
-} elseif ($subAction == "withdraw") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subMoney, $proMoney, ratioCalc($subEconVal, $proEconVal), $subUseWeapon);
-    $subMoney = $msgBox['debit'];
-    $proMoney = $msgBox['credit'];
 }
