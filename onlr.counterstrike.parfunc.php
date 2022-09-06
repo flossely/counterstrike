97893a259efcff6404e841e000fc27179a1bd43d
Karl August Nestor <jesuscaesar@outlook.com>
2022-09-06 02:51:08 +0000

    Latest update

--- onlr.counterstrike.parfunc.php
+++ onlr.counterstrike.parfunc.php
@@ -7,24 +7,20 @@ $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "strike") {
     if ($objUseShield !== null) {
         $objRating -= $subForce + $objShield;
-        $subRating += $subForce - $objShield;
         $subScore += $subForce - $objShield;
         echo $turnNum." : ".$subFullName.' '.$subForceType." (".$subForce."/".$objShield.") ".$objFullName."<br>";
     } else {
         $objRating -= $subForce;
-        $subRating += $subForce;
         $subScore += $subForce;
         echo $turnNum." : ".$subFullName.' '.$subForceType." (".$subForce.") ".$objFullName."<br>";
     }
 } elseif ($subAction == "melee") {
     if ($objUseShield !== null) {
         $objRating -= $subMeleeForce + $objShield;
-        $subRating += $subMeleeForce - $objShield;
         $subScore += $subMeleeForce - $objShield;
         echo $turnNum." : ".$subFullName.' '.$subMeleeType." (".$subMeleeForce."/".$objShield.") ".$objFullName."<br>";
     } else {
         $objRating -= $subMeleeForce;
-        $subRating += $subMeleeForce;
         $subScore += $subMeleeForce;
         echo $turnNum." : ".$subFullName.' '.$subMeleeType." (".$subMeleeForce.") ".$objFullName."<br>";
     }
