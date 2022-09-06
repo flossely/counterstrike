ec2a67b6ff072ecf98ee2576778c5e6e9a4b51e6
Karl August Nestor <jesuscaesar@outlook.com>
2022-09-06 02:35:32 +0000

    Latest update

--- onlc.counterstrike.parfunc.php
+++ onlc.counterstrike.parfunc.php
@@ -5,15 +5,15 @@ $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "weapon") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseWeapon);
+    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseWeapon, $zoneArr);
     $objMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
 } elseif ($subAction == "melee") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseMelee);
+    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseMelee, $zoneArr);
     $objMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
 } elseif ($subAction == "shield") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseShield);
+    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseShield, $zoneArr);
     $objMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
 }
